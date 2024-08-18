3ab83e569954f12e1d7d209d251f38b5b2b95476
uzhojiakbar <uzhojiakbar3@gmail.com>
2024-08-18 08:44:13 +0500

    Login Done & build

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import html from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    svgr(),
-    react(),
-    html({
-      inject: {
-        injectTo: "head",
-        // CSP header-ni qo'shish uchun meta teg
-        inject: `<meta http-equiv="Content-Security-Policy" content="script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';">`,
-      },
-    }),
-  ],
+  plugins: [react(), svgr()],
 });
